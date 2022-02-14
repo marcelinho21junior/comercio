9447f1afdd820e9fda59c9d997b18f80a630552b
marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2022-02-13 23:38:33 -0300

    commit primeiro processo

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+//css
+import './Home.css'
+
 export default function Home() {
   return (
     <div className="App">
       <h1>Home</h1>
+      <h1>teste</h1>
     </div>
   );
 }
